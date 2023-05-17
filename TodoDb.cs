25a66de186f63c67a871c7642806aa6c157246c4
john65 <joaovini2667@gmail.com>
2023-05-17 15:39:59 -0300

    TodoAPI: WebAPI com conceito de API Minima, versao: 0.0.1

---
+++ TodoDb.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TodoAPI;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
