11eb471c4f37eceeb3e9bf0421bdc3dd749170f3
john65 <joaovini2667@gmail.com>
2023-05-18 10:33:36 -0300

    Atualização: Nova estrutura para os métodos CRUD - Adição de comentarios essenciais ao arquivo: Program.cs

--- Program.cs
+++ Program.cs
@@ -6,50 +6,82 @@ builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList")
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/todoitems", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
+//URL padrão que se repetia em todas as requisições salva como valor de variavel para ser usada nas requisições
+RouteGroupBuilder todoItemsUrl = app.MapGroup("/todoitems");
 
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
-    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+todoItemsUrl.MapGet("/", GetAllTodos);
+todoItemsUrl.MapGet("/complete", GetCompleteTodos);
+todoItemsUrl.MapGet("/{id}", GetTodo);
+todoItemsUrl.MapPost("/", CreateTodo);
+todoItemsUrl.MapPut("/{id}", UpdateTodo);
+todoItemsUrl.MapDelete("/{id}", DeleteTodo);
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id)
-        is Todo todo
-            ? Results.Ok(todo)
-            : Results.NotFound());
+//Método para rodar o app(API)
+app.Run();
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+//Método de requisição GET All(Obter todos valores)
+static async Task<IResult> GetAllTodos(TodoDb db)
 {
-    db.Todos.Add(todo);
+    return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
+}
+
+//Método de requisição GET Complete(Obter apens valores onde "complete" for true)
+static async Task<IResult> GetCompleteTodos(TodoDb db)
+{
+    return TypedResults.Ok(
+        await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync()
+    );
+}
+
+//Método de requisição GET(Obter único valor via ID)
+static async Task<IResult> GetTodo(int id, TodoDb db)
+{
+    return await db.Todos.FindAsync(id) is Todo todo
+        ? TypedResults.Ok(new TodoItemDTO(todo))
+        : TypedResults.NotFound();
+}
+
+//Método de requisição POST(Criar novos dados)
+static async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
+{
+    var todoItem = new Todo { IsComplete = todoItemDTO.IsComplete, Name = todoItemDTO.Name };
+
+    db.Todos.Add(todoItem);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/todoitems/{todo.Id}", todo);
-});
+    todoItemDTO = new TodoItemDTO(todoItem);
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+    return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTO);
+}
+
+//Método de requisição UPDATE(Atualizar dados existentes de acordo com a ID inserida)
+static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDb db)
 {
     var todo = await db.Todos.FindAsync(id);
 
-    if (todo is null) return Results.NotFound();
+    if (todo is null)
+        return TypedResults.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = todoItemDTO.Name;
+    todo.IsComplete = todoItemDTO.IsComplete;
 
     await db.SaveChangesAsync();
 
-    return Results.NoContent();
-});
+    return TypedResults.NoContent();
+}
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+//Método de requisição DELETE(Deletar dados de acordo com a ID inserida)
+static async Task<IResult> DeleteTodo(int id, TodoDb db)
 {
     if (await db.Todos.FindAsync(id) is Todo todo)
     {
         db.Todos.Remove(todo);
         await db.SaveChangesAsync();
-        return Results.Ok(todo);
+
+        TodoItemDTO todoItemDTO = new TodoItemDTO(todo);
+
+        return TypedResults.Ok(todoItemDTO);
     }
 
-    return Results.NotFound();
-});
-
-app.Run();
+    return TypedResults.NotFound();
+}
--- Todo.cs
+++ Todo.cs
@@ -5,5 +5,6 @@ namespace TodoAPI
         public int Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
     }
 }
